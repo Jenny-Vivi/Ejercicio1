fa9d7aaec20b940eb8ca593a491d2746e9b6ca24
Jenny-Vivi <153759617+Jenny-Vivi@users.noreply.github.com>
2024-08-12 17:04:41 -0500

    Commit en el submódulo

--- src/Seleccion.java
+++ src/Seleccion.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Seleccion {
     public static void main(String[] args) {
-        Scanner tecla = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
         System.out.println("Ingresa el tamanio del arreglo");
-        int tamanio = tecla.nextInt();
+        int tamanio = teclado.nextInt();
         int[] numeros = new int[tamanio];
         for (int i = 0; i < tamanio; i++) {
             System.out.println("Ingresa los numeros  [" + i + "]");
-            numeros[i] = tecla.nextInt();
+            numeros[i] = teclado.nextInt();
         }
 
     }
